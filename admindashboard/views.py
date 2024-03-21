924e74320cb087ccb03ba01f0a95e5bba7443bc5
chandachewe10 <chewec03@gmail.com>
2024-03-21 09:56:16 +0200

    Add Login require for Dashboard

--- admindashboard/views.py
+++ admindashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='login')
 def dashboard(request):
+    # Your view logic goes here
     return render(request, 'dashboard.html')
