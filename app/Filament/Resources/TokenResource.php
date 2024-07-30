a007852846e2cb0e9bbd380a97a0e066a6f0a997
Admin <chewec03@gmail.com>
2024-07-30 22:38:10 +0200

    Added Forms for Tokens

---
+++ app/Filament/Resources/TokenResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TokenResource\Pages;
+use App\Filament\Resources\TokenResource\RelationManagers;
+use App\Models\User as Token;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TokenResource extends Resource
+{
+    protected static ?string $model = Token::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-key';
+    protected static ?string $navigationGroup = 'Developers';
+    protected static ?string $modelLabel = 'Token';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('remember_token')
+                ->label('Token Name')
+                ->prefixIcon('heroicon-o-key')
+                    ->minLength(2)
+                    ->maxLength(100)
+                    ->columnSpan(2),
+                    ,
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageTokens::route('/'),
+        ];
+    }
+}
