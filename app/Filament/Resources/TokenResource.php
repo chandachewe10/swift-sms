346e2d57626e2c7eccb74ef40ac115c7093c606a
Admin <chewec03@gmail.com>
2024-07-31 00:03:52 +0200

    Added Forms for Tokens

--- app/Filament/Resources/TokenResource.php
+++ app/Filament/Resources/TokenResource.php
@@ -43,12 +43,12 @@ class TokenResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                // Tables\Actions\EditAction::make(),
+                // Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    // Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
     }
