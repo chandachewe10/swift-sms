816d12872c644cde013ca8df824cf33e26d1569e
Admin <chewec03@gmail.com>
2024-09-15 17:39:04 +0200

    Reverse For token

--- app/Filament/Resources/TokenResource.php
+++ app/Filament/Resources/TokenResource.php
@@ -36,11 +36,7 @@ class TokenResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
-        ->modifyQueryUsing(function (Builder $query) { 
-           
-            return $query->where('company_id', auth()->user()->user_id); 
-        
-    }) 
+       
             ->columns([
                 //
             ])
