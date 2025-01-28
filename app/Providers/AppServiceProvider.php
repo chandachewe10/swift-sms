c6b0db018544ada2040573cc3bbf2a1632a3e561
chandachewe10 <chewec03@gmail.com>
2025-01-29 00:31:28 +0200

    Change .env.example db to mysql as default

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
        
-            User::observe(UserObserver::class);
+           User::observe(UserObserver::class);
         
     }
 }
