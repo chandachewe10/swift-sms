e4025d8a24304198fa28b987d50c1579b7ce812f
chandachewe10 <chewec03@gmail.com>
2025-02-04 00:11:54 +0200

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\MessagesAPI;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware([
+    'auth:sanctum',
+])->group(function () {
+    Route::get('send_message', [MessagesAPI::class, 'store']);
+
+});
